e94d111a6bbac1f5924b7014ba6b497c17f368fc
Connor <109663367+cwmasonRollTide@users.noreply.github.com>
2024-11-16 19:22:08 +0000

    implementations for current user service (stashed local)

--- Backend/Inventory/Services/CurrentUserService.cs
+++ Backend/Inventory/Services/CurrentUserService.cs
@@ -7,3 +7,30 @@ public interface ICurrentUserService
     string? GetUserEmail();
 }
 
+public class CurrentUserService : ICurrentUserService
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    public long? GetCurrentUserId()
+    {
+        var userId = _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+        return userId != null ? long.Parse(userId) : (long?)null;
+    }
+
+    public long? GetCurrentOrganizationId()
+    {
+        var organizationId = _httpContextAccessor.HttpContext?.User?.FindFirstValue("OrganizationId");
+        return organizationId != null ? long.Parse(organizationId) : (long?)null;
+    }
+
+    public string? GetUserEmail()
+    {
+        return _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.Email);
+    }
+}
+
